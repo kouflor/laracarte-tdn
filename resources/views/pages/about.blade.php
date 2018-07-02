b7c2bf0b501acc7ad21070bdf56a2ecb924b3785
kouakou florentin konan <kouaflorentin@live.fr>
2018-07-02 18:00:16 +0000

    Setup and add static pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.default')
+
+@section('content')
+       <h1 class="text-center">About</h1>
+
+	<div class="container">What is Laracarte?	
+		<p>Laracarte is a clone app of <a href="https://Laramap.com" target="_blank"> Laramap.com.</a></p>
+	<div class="row">
+	<div class="col-md-6">
+		<p class="alert alert-warning">
+			<i class="fa fa-warning"></i>
+			 <strong>This app has been built by <a href="https://twitter.com/etsmo" target="_blank"> @etsmo</a> for learning purposes.</strong>
+		</p>
+		<p>Feel free to help to improve the <a href="https://github.com/mercuryseries/laracarte" target="_blank"> source code.</a></p>
+		
+		</div>
+		</div>		
+		<hr>
+		<div>
+		
+		
+		<h2>What is Laramap?</h2>
+		<p>Laramap is the website by which Laracarte was inspired :).</p>
+		<p>More info <a href="#"> here.</a></p>
+		</div>
+		<hr>
+		<h2>Which tools and services are used in Laracarte?</h2>
+		<p>Basically it's built on Laravel & Bootstrap. But there's a bunch of services used for email and other sections.</p>
+		<ul>
+			<li>Laravel</li>
+			<li>Bootstrap</li>
+			<li>Amazon S3</li>
+			<li>Mandrill</li>
+			<li>Google Maps</li>
+			<li>Gravatar</li>
+			<li>Antony Martin's Geolocation Package</li>
+			<li>Michel Fortin's Markdown Parser Package</li>
+			<li>Heroku</li>
+		</ul>
+
+
+		</div>
+			
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1 class="text-center">Ma carte ICI!!!!!!</h1>
+@stop

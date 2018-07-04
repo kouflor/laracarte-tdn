ebd5b9f2962123a151ca7c520f8825a69f28c338
kouakou florentin konan <kouaflorentin@live.fr>
2018-07-04 15:52:13 +0000

    changer le nom de l'application

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,8 @@
 @section('content')
        <h1 class="text-center">About</h1>
 
-	<div class="container">What is Laracarte?	
-		<p>Laracarte is a clone app of <a href="https://Laramap.com" target="_blank"> Laramap.com.</a></p>
+	<div class="container">What is {{ config('app.name') }}?	
+		<p>{{ config('app.name') }} is a clone app of <a href="https://Laramap.com" target="_blank"> Laramap.com.</a></p>
 	<div class="row">
 	<div class="col-md-6">
 		<p class="alert alert-warning">
@@ -20,11 +20,11 @@
 		
 		
 		<h2>What is Laramap?</h2>
-		<p>Laramap is the website by which Laracarte was inspired :).</p>
+		<p>Laramap is the website by which {{ config('app.name') }} was inspired :).</p>
 		<p>More info <a href="#"> here.</a></p>
 		</div>
 		<hr>
-		<h2>Which tools and services are used in Laracarte?</h2>
+		<h2>Which tools and services are used in {{ config('app.name') }}?</h2>
 		<p>Basically it's built on Laravel & Bootstrap. But there's a bunch of services used for email and other sections.</p>
 		<ul>
 			<li>Laravel</li>
